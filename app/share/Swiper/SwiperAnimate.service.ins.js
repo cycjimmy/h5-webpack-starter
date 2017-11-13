337e05c4cf1b3e6cd07f84bcce2f25c5ff50ff98
cycjimmy <cycjimmy@gmail.com>
2017-11-13 13:22:06 +0800

    v0.2.5
    use swiper 4

--- app/share/Swiper/SwiperAnimate.service.ins.js
+++ app/share/Swiper/SwiperAnimate.service.ins.js
@@ -16,108 +16,100 @@ export default class SwiperAnimateServiceIns {
     if (_instance()) {
       return _instance();
     }
-    this.allBoxes = null;
-    this.isBusy = false;
+    this.allBoxes = [];
     _instance(this);
   };
 
-  init() {
-    this.allBoxes = nodeListToArray(window.document.documentElement.querySelectorAll(".ani"));
-  };
+  _clear() {
+    return Promise.all(
+      this.allBoxes.map(el => {
+        return new Promise(resolve => {
+          if (el.attributes['swiper-animate-style-cache']) {
+            el.setAttribute('style', el.attributes['swiper-animate-style-cache'].value);
+            el.style.visibility = 'hidden';
+            el.classList.remove('animated');
 
-  clear() {
-    return new Promise((resolve, reject) => {
-      if (!this.isBusy) {
-        if (!this.allBoxes) {
-          this.init();
-        }
-        this.isBusy = true;
-        resolve();
-      } else {
-        console.log('busy');
-        setTimeout(() => {
-          reject();
-        }, 100);
-      }
-    })
-      .then(
-        () => {
-          return Promise.all(
-            this.allBoxes.map(el => {
-              return new Promise(resolve => {
-                if (el.attributes["swiper-animate-style-cache"]) {
-                  el.setAttribute("style", el.attributes["swiper-animate-style-cache"].value);
-                  el.style.visibility = "hidden";
-                  el.classList.remove('animated');
-
-                  if (el.attributes["swiper-animate-effect"]) {
-                    el.classList.remove(el.attributes["swiper-animate-effect"].value);
-                  }
-                }
-                setTimeout(() => resolve(), 0);
-              });
-            }),
-          );
-        },
-        () => {
-          return this.clear();
+            if (el.attributes['swiper-animate-effect']) {
+              el.classList.remove(el.attributes['swiper-animate-effect'].value);
+            }
+          }
+          setTimeout(() => resolve(), 0);
         });
+      }),
+    );
   };
 
-  cache() {
-    if (!this.allBoxes) {
-      this.init();
+  _cache() {
+    // has cached
+    if (this.allBoxes.length) {
+      return Promise.resolve();
     }
 
-    this.allBoxes.forEach(el => {
-      if (el.attributes["style"]) {
-        el.setAttribute("swiper-animate-style-cache", el.attributes["style"].value);
-      } else {
-        el.setAttribute("swiper-animate-style-cache", " ");
-        el.style.visibility = "hidden";
-      }
-    });
+    // start cache
+    return new Promise(resolve => {
+      this._initAllBoxes();
+      setTimeout(() => resolve(), 0);
+    })
+      .then(() => {
+        return new Promise(resolve => {
+          this.allBoxes.forEach(el => {
+            if (el.attributes['style']) {
+              el.setAttribute('swiper-animate-style-cache', el.attributes['style'].value);
+            } else {
+              el.setAttribute('swiper-animate-style-cache', ' ');
+              el.style.visibility = 'hidden';
+            }
+          });
+
+          setTimeout(() => resolve(), 0);
+        });
+      });
   };
 
   animate(swiper) {
-    this.clear()
+    return Promise.resolve()
+      .then(() => this._cache())
+      .then(() => this._clear())
       .then(() => {
         let
-          activeBoxes = nodeListToArray(swiper.slides[swiper.realIndex].querySelectorAll(".ani"))
+          activeBoxes = nodeListToArray(swiper.slides[swiper.realIndex].querySelectorAll('.ani'))
         ;
 
         return Promise.all(activeBoxes.map(el => {
           let
             style
-            , effect = el.attributes["swiper-animate-effect"]
-            ? el.attributes["swiper-animate-effect"].value
-            : ""
-            , duration = el.attributes["swiper-animate-duration"]
-            ? el.attributes["swiper-animate-duration"].value
-            : ""
-            , delay = el.attributes["swiper-animate-delay"]
-            ? el.attributes["swiper-animate-delay"].value
-            : ""
+            , effect = el.attributes['swiper-animate-effect']
+            ? el.attributes['swiper-animate-effect'].value
+            : ''
+            , duration = el.attributes['swiper-animate-duration']
+            ? el.attributes['swiper-animate-duration'].value
+            : ''
+            , delay = el.attributes['swiper-animate-delay']
+            ? el.attributes['swiper-animate-delay'].value
+            : ''
           ;
 
-          el.style.visibility = "visible";
+          el.style.visibility = 'visible';
           el.classList.add(effect, 'animated');
 
-          style = el.attributes["style"].value;
+          style = el.attributes['style'].value;
 
           if (duration) {
-            style += "animation-duration:" + duration + ";-webkit-animation-duration:" + duration + ";";
+            style += 'animation-duration:' + duration + ';-webkit-animation-duration:' + duration + ';';
           }
 
           if (delay) {
-            style += "animation-delay:" + delay + ";-webkit-animation-delay:" + delay + ";";
+            style += 'animation-delay:' + delay + ';-webkit-animation-delay:' + delay + ';';
           }
 
-          el.setAttribute("style", style);
+          el.setAttribute('style', style);
         }));
-      })
-      .then(() => {
-        this.isBusy = false;
       });
+  };
+
+  _initAllBoxes() {
+    if (!this.allBoxes.length) {
+      this.allBoxes = nodeListToArray(window.document.documentElement.querySelectorAll('.ani'));
+    }
   };
 };
