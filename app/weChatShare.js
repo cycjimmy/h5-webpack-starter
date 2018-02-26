662892da2302ab4b66f0a6573ab59517931956b2
cycjimmy <cycjimmy@gmail.com>
2018-02-26 15:30:37 +0800

    v0.3.1
    update dependencies;
    use OfflinePlugin;
    use weixin-share;

--- app/weChatShare.js
+++ app/weChatShare.js
@@ -3,7 +3,7 @@
  */
 
 // service
-import WechatSDKServiceIns from './share/weChatSDK/WechatSDK.service.ins';
+import WxShare from 'weixin-share';
 
 // picture
 import weChatSharePic from '../static/images/noUrl/weChatSharePic.png';
@@ -14,30 +14,16 @@ let
     link: window.location.href.replace(/(\?|#).*/g, ''),
     title: 'H5 Webpack Starter',
     desc: 'H5 webpack starter for webpack, gulp, es6/7, sass, pug, swiper, jsmpeg...',
-  };
-
-// WechatSDK
-let
-  defaultShare = () => {
-    new WechatSDKServiceIns().onShare(defaultData);
   }
 
-  , asyncShare = ({
-                    imgUrl = defaultData.imgUrl,
-                    link = defaultData.link,
-                    title = defaultData.title,
-                    desc = defaultData.desc,
-                  }) => {
-    new WechatSDKServiceIns().ready().onShareAsync({
-      imgUrl,
-      link,
-      title,
-      desc,
-    });
+  , wxShareConfig = {
+    appId: 'appId',
+    timestamp: 'timestamp',
+    nonceStr: 'nonceStr',
+    signature: 'signature'
   }
 ;
 
-export {
-  defaultShare,
-  asyncShare,
-};
+new WxShare()
+  .config(wxShareConfig)
+  .share(defaultData);
