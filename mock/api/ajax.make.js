d9137d83742f94e5ffd40e724edf1e1b41bc0e2d
cycjimmy <cycjimmy@gmail.com>
2018-12-05 16:15:30 +0800

    v0.7.4

--- mock/api/ajax.make.js
+++ mock/api/ajax.make.js
@@ -1,8 +1,4 @@
 /**
- * Created by cyc on 2017/2/8.
- */
-
-/**
  * mock ajax api
  * @param apiName
  * @param data
@@ -10,29 +6,41 @@
  * @returns {function(*, *=, *=)}
  */
 module.exports = ({
-  apiName,
-  data = null,
-  timeout = 200,
-}) => {
-  return ()=>{
-    apiName = '['+apiName+']';
+                    apiName,
+                    data = null,
+                    timeout = 200,
+                  }) => {
+  return () => {
+    apiName = '[' + apiName + ']';
 
     return (req, res, next) => {
-
       let remoteAddress = req.headers['x-forwarded-for'] ||
         req.connection.remoteAddress ||
         req.socket.remoteAddress ||
         req.connection.socket.remoteAddress
       ;
 
-      //设置响应头文件
+      // Set Header
       res.setHeader("Access-Control-Allow-Origin", "*");
       res.setHeader("Content-Type", "application/json;charset=UTF-8");
 
-      //收到浏览器数据
-      req.on('data', e => {
+      if (req.method === 'GET') {
+        console.log(
+          apiName + ' Receive a require' + '\n' +
+          apiName + ' -----------------' + '\n' +
+          apiName + ' Method: ' + req.method + '\n' +
+          apiName + ' RemoteAddress: ' + remoteAddress + '\n' +
+          apiName + ' Data: ' + req.query + '\n'
+        );
 
-        //打印信息
+        setTimeout(() => {
+          res.end(JSON.stringify(data));
+          next();
+        }, timeout);
+      }
+
+      // Receive the browser data
+      req.on('data', e => {
         console.log(
           apiName + ' Receive a require' + '\n' +
           apiName + ' -----------------' + '\n' +
@@ -41,16 +49,13 @@ module.exports = ({
           apiName + ' Data: ' + e + '\n'
         );
 
-        //延迟执行
         setTimeout(() => {
           res.end(JSON.stringify(data));
           next();
         }, timeout);
       });
 
-      //错误处理
       req.on('err', err => {
-        //打印错误
         console.log(
           apiName + ' Error: ' + err + '\n'
         );
