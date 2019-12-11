54f6efe3b757aca91dc4d9ac606a2465b065b06e
cycjimmy <cycjimmy@gmail.com>
2019-12-11 14:47:27 +0800

    chore(*): move initialization task to load.js

--- app/main.js
+++ app/main.js
@@ -1,27 +1,5 @@
 import FastClick from 'fastclick';
 import webInitialize from './webInitialize.afunc';
-import preventDefault from '@cycjimmy/awesome-js-funcs/event/preventDefault';
-
-if (DEVELOPMENT) {
-  console.log('Development Mode');
-  console.log(require('@cycjimmy/awesome-js-funcs/handheld').getBrowserInfo());
-}
-
-if (PRODUCTION) {
-  console.log('Production Mode');
-}
-
-import * as OfflinePluginRuntime from 'offline-plugin/runtime';
-
-if (!DEVELOPMENT) {
-  OfflinePluginRuntime.install();
-}
-
-// contextMenu preventDefault
-document.addEventListener('contextmenu', preventDefault);
-
-// fix ios native scrolling
-document.body.addEventListener('touchmove', preventDefault, {passive: false});
 
 // web page init
 document.addEventListener('DOMContentLoaded', () => {
