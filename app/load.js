714c9e883affa4db2cb2d7e30847e2f10dac2772
cycjimmy <cycjimmy@gmail.com>
2019-12-11 14:47:27 +0800

    chore(*): move initialization task to load.js

--- app/load.js
+++ app/load.js
@@ -1,8 +1,32 @@
 // global css
 import './theme/main.scss';
 
+// services
+import preventDefault from '@cycjimmy/awesome-js-funcs/event/preventDefault';
+
 // polyfill
 import './polyfill';
+
+// offline runtime
+if (!DEVELOPMENT) {
+  const OfflinePluginRuntime = require('offline-plugin/runtime');
+  OfflinePluginRuntime.install();
+}
+
+if (DEVELOPMENT) {
+  console.log('Development Mode');
+  console.log(require('@cycjimmy/awesome-js-funcs/handheld').getBrowserInfo());
+}
+
+if (PRODUCTION) {
+  console.log('Production Mode');
+}
+
+// contextMenu preventDefault
+document.addEventListener('contextmenu', preventDefault);
+
+// fix ios native scrolling
+document.body.addEventListener('touchmove', preventDefault, {passive: false});
 
 // component
 import LoadingComponent from './loadingComponent/Loading.component';
