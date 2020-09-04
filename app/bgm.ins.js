d5e0a4aaea268dcda409d7b967c167af1d29698f
cycjimmy <cycjimmy@gmail.com>
2020-09-04 08:13:04 +0000

    Deploying to release from  @ 7d286df5d9c7d98e9b5d4d8c68db5c04c252fe7f 🚀

--- app/bgm.ins.js
+++ app/bgm.ins.js
@@ -1,14 +1,9 @@
-// services
 import {h5Pages} from '@cycjimmy/h5-pages';
 import h5AudioControls from '@cycjimmy/h5-audio-controls';
-import CreateInstance from '@cycjimmy/awesome-js-funcs/designPattern/CreateInstance';
 
 import staticFiles from './staticFiles';
-
-const instance = new CreateInstance();
 
 export default h5AudioControls(staticFiles.bgm, {
   context: h5Pages.root,
   positionType: 'absolute',
 });
-
