03d04312315e1cb78fab2283f1bb6db5b309b867
cycjimmy <cycjimmy@gmail.com>
2021-11-16 10:23:12 +0000

    Deploying to release from @ cycjimmy/h5-webpack-starter@3690bca25bca69884c1440382d52b9d6fccaaa4d 🚀

--- app/load.js
+++ app/load.js
@@ -7,10 +7,17 @@ import promptMobile from './popups/promptMobile/promptMobile.component.ins';
 // polyfill
 import './polyfill';
 
-// offline runtime
+// Registering Service Worker
 if (!DEVELOPMENT) {
-  const OfflinePluginRuntime = require('@lcdp/offline-plugin/runtime');
-  OfflinePluginRuntime.install();
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('./service-worker.js').then(registration => {
+        console.log('SW registered: ', registration);
+      }).catch(registrationError => {
+        console.log('SW registration failed: ', registrationError);
+      });
+    });
+  }
 }
 
 if (DEVELOPMENT) {
