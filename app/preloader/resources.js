68fbf07181770c05f199dce7aecb1bdede759f5c
cycjimmy <>
2021-11-23 07:33:55 +0000

    Deploying to release from @ cycjimmy/h5-webpack-starter@bb87a03b7a150d4595acd70ea0f56a1198aa624f 🚀

--- app/preloader/resources.js
+++ app/preloader/resources.js
@@ -1,6 +1,17 @@
+import entries from '@cycjimmy/awesome-js-funcs/esm/object/entries';
 import staticFiles from '../staticFiles';
 
 export default [
   // require('../../static/images/').default,
-  staticFiles.bgm,
+  require('../../static/images/noUrl/weChatSharePic.png').default,
+
+  ...(() => {
+    const aStaticFiles = [];
+    entries(staticFiles).forEach(([k, v]) => {
+      if (Object.prototype.hasOwnProperty.call(staticFiles, k)) {
+        aStaticFiles.push(v);
+      }
+    });
+    return aStaticFiles;
+  })(),
 ];
